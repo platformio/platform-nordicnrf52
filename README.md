8ea27b1c972e2689395eacc026c9c6100c05109d
Valeriy Koval <valeros@bigmir.net>
2017-05-25 19:56:44 +0300

    Initial implementation

---
+++ README.md
@@ -0,0 +1,20 @@
+# Nordic nRF52: development platform for [PlatformIO](http://platformio.org)
+[![Build Status](https://travis-ci.org/platformio/platform-nordicnrf52.svg?branch=develop)](https://travis-ci.org/platformio/platform-nordicnrf52)
+[![Build status](https://ci.appveyor.com/api/projects/status/e6cno5podkuha0ox/branch/develop?svg=true)](https://ci.appveyor.com/project/ivankravets/platform-nordicnrf52/branch/develop)
+
+The nRF52 Series are built for speed to carry out increasingly complex tasks in the shortest possible time and return to sleep, conserving precious battery power. They have a Cortex-M4F processor and are the most capable Bluetooth Smart SoCs on the market.
+
+* [Home](http://platformio.org/platforms/nordicnrf52) (home page in PlatformIO Platform Registry)
+* [Documentation](http://docs.platformio.org/page/platforms/nordicnrf52.html) (advanced usage, packages, boards, frameworks, etc.)
+
+# Usage
+
+1. [Install PlatformIO Core](http://docs.platformio.org/page/core.html)
+2. Install Nordic nRF52 development platform:
+```bash
+# install the latest stable version
+> platformio platform install nordicnrf52
+
+# install development version
+> platformio platform install https://github.com/platformio/platform-nordicnrf52.git
+```
---
+++ builder/main.py
@@ -0,0 +1,167 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os.path import join
+
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
+                          DefaultEnvironment)
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    GDB="arm-none-eabi-gdb",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rcs"],
+
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-g",   # include debugging info (so errors include line numbers)
+        "-Os",  # optimize for size
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-Wall",
+        "-mthumb",
+        "-nostdlib"
+    ],
+
+    CXXFLAGS=[
+        "-fno-rtti",
+        "-fno-exceptions"
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-Wl,--gc-sections,--relax",
+        "-mthumb"
+    ],
+
+    LIBS=["c", "gcc", "m"],
+
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGNAME="firmware",
+    PROGSUFFIX=".elf"
+)
+
+if "BOARD" in env:
+    env.Append(
+        CCFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ],
+        LINKFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ]
+    )
+
+env.Append(
+    ASFLAGS=env.get("CCFLAGS", [])[:],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        ),
+        MergeHex=Builder(
+            action=env.VerboseAction(" ".join([
+                join(platform.get_package_dir("tool-sreccat") or "",
+                     "srec_cat"),
+                "$SOFTDEVICEHEX",
+                "-intel",
+                "$SOURCES",
+                "-intel",
+                "-o",
+                "$TARGET",
+                "-intel",
+                "--line-length=44"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_firm = join("$BUILD_DIR", "firmware.hex")
+else:
+    target_elf = env.BuildProgram()
+    if "SOFTDEVICEHEX" in env:
+        target_firm = env.MergeHex(
+            join("$BUILD_DIR", "firmware"),
+            env.ElfToHex(join("$BUILD_DIR", "userfirmware"), target_elf)
+        )
+    else:
+        target_firm = env.ElfToHex(join("$BUILD_DIR", "firmware"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload by default .bin file
+#
+
+target_upload = env.Alias(
+    "upload", target_firm,
+    [env.VerboseAction(env.AutodetectUploadPort, "Looking for upload disk..."),
+     env.VerboseAction(env.UploadToDisk, "Uploading $SOURCE")])
+AlwaysBuild(target_upload)
+
+#
+# Default targets
+#
+
+Default([target_buildprog, target_size])
