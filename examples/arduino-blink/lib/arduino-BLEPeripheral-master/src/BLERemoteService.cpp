8afb749a9816b29725c04ce63d748e3ce4db423c
Valerii Koval <valeros@bigmir.net>
2017-08-03 13:19:40 +0300

    Update CI configs with new examples

--- examples/arduino-blink/lib/arduino-BLEPeripheral-master/src/BLERemoteService.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Sandeep Mistry. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#include "BLERemoteService.h"
-
-BLERemoteService::BLERemoteService(const char* uuid) :
-  BLERemoteAttribute(uuid, BLETypeService)
-{
-}
--- examples/arduino-blink/lib/arduino-BLEPeripheral-master/src/utility/README.md
+++
@@ -1,3 +0,0 @@
-These files are from the [@NordicSemiconductor](https://github.com/NordicSemiconductor)[ble-sdk-arduino](https://github.com/NordicSemiconductor/ble-sdk-arduino).
-
-Some file have been slightly modified.
