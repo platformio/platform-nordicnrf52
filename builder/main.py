08e15ee516967363d93f2f0db05080d58fb5fc7c
Ivan Kravets <me@ikravets.com>
2019-06-13 21:07:22 +0300

    Automatically configure openOCD scripts folder

--- builder/main.py
+++ builder/main.py
@@ -17,8 +17,8 @@ from platform import system
 from os import makedirs
 from os.path import isdir, join
 
-from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
-                          DefaultEnvironment)
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
@@ -305,14 +305,23 @@ elif upload_protocol.startswith("jlink"):
     upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
 elif upload_protocol in debug_tools:
+    openocd_args = [
+        "-d%d" % (2 if int(ARGUMENTS.get("PIOVERBOSE", 0)) else 1)
+    ]
+    openocd_args.extend(
+        debug_tools.get(upload_protocol).get("server").get("arguments", []))
+    openocd_args.extend([
+        "-c", "program {$SOURCE} %s verify reset; shutdown;" %
+        board.get("upload.offset_address", "")
+    ])
+    openocd_args = [
+        f.replace("$PACKAGE_DIR",
+                  platform.get_package_dir("tool-openocd") or "")
+        for f in openocd_args
+    ]
     env.Replace(
         UPLOADER="openocd",
-        UPLOADERFLAGS=["-s", platform.get_package_dir("tool-openocd") or ""] +
-        debug_tools.get(upload_protocol).get("server").get("arguments", []) + [
-            "-c",
-            "program {$SOURCE} verify reset %s; shutdown;" %
-            env.BoardConfig().get("upload.offset_address", "")
-        ],
+        UPLOADERFLAGS=openocd_args,
         UPLOADCMD="$UPLOADER $UPLOADERFLAGS")
     upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
